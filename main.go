f107832ec89fc1933750476c3b861eab26325131
Brian Newsom <brian.newsom@colorado.edu>
2016-04-06 00:39:12 -0600

    Handle directories, globs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"path/filepath"
 	//	"io/ioutil"
 	"crypto/rand"
 	"errors"
@@ -15,26 +16,71 @@ func main() {
 	overwrites := *flag.Int("overwrites", 3, "How many times to overwrite the file with random data before deleting")
 	flag.Parse()
 
-	fName, err := parseArgs(os.Args)
+	pattern, err := parseArgs(os.Args)
 	if err != nil {
 		fatal(err)
 	}
 
+	// Ensure we're starting from the correct dir
+
+	fs, err := filepath.Glob(pattern)
+	if err != nil {
+		fatal(err)
+	}
+
+	for _, f := range fs {
+		openFileAndKillRecursive(f, overwrites)
+	}
+}
+
+func openFileAndKillRecursive(fName string, overwrites int) error {
 	f, err := os.OpenFile(fName, os.O_WRONLY, 0000)
-	if err != nil {
-		fatal(err)
-	}
 
-	for i := 0; i < overwrites; i++ {
-		err = overwriteFile(f)
+	if err != nil {
+		// TODO: Find a cleaner way to do this
+		err = handleDir(fName, overwrites)
 		if err != nil {
-			fatal(err)
+			return err
+		} else {
+			return nil
 		}
 	}
 
-	removeFile(f)
+	defer f.Close()
+
+	killFile(f, overwrites)
 
 	fmt.Printf("Successfully removed file %s", f.Name())
+	return nil
+}
+
+func handleDir(dName string, overwrites int) error {
+	d, err := os.Open(dName)
+
+	if err != nil {
+		return err
+	}
+
+	defer d.Close()
+
+	files, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		err = openFileAndKillRecursive(dName+"."+string(filepath.Separator)+file, overwrites)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = os.Remove(dName)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func fatal(err error) {
@@ -42,7 +88,14 @@ func fatal(err error) {
 	os.Exit(1)
 }
 
-func removeFile(f *os.File) error {
+func killFile(f *os.File, overwrites int) error {
+	for i := 0; i < overwrites; i++ {
+		err := overwriteFile(f)
+		if err != nil {
+			fatal(err)
+		}
+	}
+
 	return os.Remove(f.Name())
 }
 
@@ -50,6 +103,10 @@ func overwriteFile(f *os.File) error {
 	fileInfo, err := f.Stat()
 	if err != nil {
 		return errors.New("Failed to retrieve file info")
+	}
+
+	if fileInfo.IsDir() {
+		fatal(errors.New("Trying to remove directory - not handled yet"))
 	}
 
 	s := fileInfo.Size()
